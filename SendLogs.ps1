b5b1b1bdd6d158c5cb987655106fe409b7e2d19e
michieldespiegelaere <michiel.despiegelaere@student.howest.be>
2017-11-17 16:27:46 +0100

    werkend script v1

--- SendLogs.ps1
+++ SendLogs.ps1
@@ -1,15 +1,43 @@
-# Initial Version to try and send data from Windows clients 
-# to the ElasticSearch server
+#powershell ise als admin openen. 1 maal Set-ExecutionPolicy Unrestricted uitvoeren. dan ist goe
 
-function getAddress () {
-  "http://192.168.10.44:9200/"
+$user = "elastic"
+$pass = "changeme"
+$pair = "${user}:${pass}"
+
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
+$base64 = [System.Convert]::ToBase64String($bytes)
+
+$basicAuthValue = "Basic $base64"
+
+
+
+$call = {
+    param($verb, $body)
+
+    $uri = "http://192.168.10.44:9200/osquery/query"
+
+    $headers = @{ 
+        'Authorization' = $basicAuthValue
+    }
+
+    Write-Host "`nCalling [$uri]" -f Green
+    if($body) {
+        if($body) {
+            Write-Host "BODY`n--------------------------------------------`n$body`n--------------------------------------------`n" -f Green
+        }
+    }
+
+    $response = Invoke-WebRequest -UseBasicParsing $uri -Method Post -Headers $headers -ContentType 'application/json' -Body $body
+
+
+    #$response = wget -Uri "$uri/$uriParams" -method $verb -Headers $headers -ContentType 'application/json' -Body $body
+    $response.Content
 }
 
-Invoke-WebRequest -UseBasicParsing getAddress(null) -
-Method POST -ContentType "application/json" -Body '{
-"user" : "elastic",
-"password" : "changeme",
-"post_date" : "'Get-Date -Format s '",
-"message" : "trying out Elasticsearch"
-}'
 
+Function post  {
+    Param($body)
+    &$call "Post" $body
+}
+
+$testBody = '{"hello":"world"}'
