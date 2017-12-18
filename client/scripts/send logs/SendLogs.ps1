d6b3f2f36a55b2a2919fd2020bdc3da5ef577e1f
Bert Goens <bertgoens@gmail.com>
2017-12-18 10:50:14 +0100

    send to index of today's date

--- client/scripts/send logs/SendLogs.ps1
+++ client/scripts/send logs/SendLogs.ps1
@@ -15,75 +15,97 @@ $base64 = [System.Convert]::ToBase64String($bytes)
 
 $basicAuthValue = "Basic $base64"
 
-$macOsqueryLogPath = "/var/log/osquery/osqueryd.results.log"
-$macScriptLogPath = '~/Desktop/SendLog.log'
+Function GetOSFilePath {
+    Param(
+        [string]$OSVersion
+    )
+    [string[]] $result = '0', '1'
+    if ($OSVersion.Equals('windows')) {
+        $result[0] = "C:\ProgramData\osquery\log\osqueryd.results.log"
+        $result[1] = "C:\SendLog\SendLog.log"
+    }
+    elseif ($OSVersion.Equals('macOs')) {
+        $result[0] = "/var/log/osquery/osqueryd.results.log"
+        $result[1] = '~/Desktop/SendLog.log'
+    }
+    elseif ($OSVersion.Equals('ubuntu')) {
+        $result[0] = "/var/log/osquery/osqueryd.results.log"
+        $result[1] = "/var/log/SendLog/testlog.log"
+    }
+    return $result
+}
 
-$windowsOsqueryLogPath = "C:\ProgramData\osquery\log\osqueryd.results.log"
-$windowsScriptLogPath = "C:\SendLog\SendLog.log"
+Function WriteLog {
+    Param(
+        [string]$loggable,
+        [string]$scriptLogPath
+    )
+  
+    $addToLog = '{"timestamp": "' + $(Get-Date -Format s) + '", "result":"' + $loggable + '"}'  
 
-$ubuntuOsqueryLogPath = "/var/log/osquery/osqueryd.results.log"
-$ubuntuScriptLogPath = "/var/log/SendLog/testlog.log"
-
-Function MakeLogObject {
-  Param(
-    [string]$loggable
-  )
-  return '{"timestamp": "' + $(Get-Date -Format s) + '", "result":"' + $loggable + '"}'
+    Add-Content $scriptLogPath $addToLog
 }
 
 Function SendToElasticSearch {
-  Param(
-    [string]$osqueryLogPath,
-    [string]$scriptLogPath
-  )
+    Param(
+        [ValidateSet('windows', 'macOs', 'ubuntu')]
+        [string] 
+        $OSVersion 
+    )
 
-  Add-Content $scriptLogPath $(MakeLogObject -loggable 'run_start') 
+    $arr = GetOSFilePath -OSVersion $OSVersion
+    $osqueryLogPath = $arr[0]
+    $scriptLogPath = $arr[1]
 
-  # Check for logs
-  if (-Not (Test-Path $osqueryLogPath)) {
-    Add-Content $scriptLogPath $(MakeLogObject -loggable 'no_file')
-    return
-  }
+    WriteLog -scriptLogPath $scriptLogPath -loggable 'run_start'
 
-  # Read logdata
-  $logContent = Get-Content -Path $osqueryLogPath # | Select-Object -last 5
+    # Check for logs
+    if (-Not (Test-Path $osqueryLogPath)) {
+        Write-Host 'no_file'
+        WriteLog -scriptLogPath $scriptLogPath -loggable 'no_file'
+        WriteLog -scriptLogPath $scriptLogPath -loggable 'run_end' 
+        return
+    }
+
+    # Read logdata
+    $logContent = Get-Content -Path $osqueryLogPath # | Select-Object -last 5
    
-  # Remove file after read
-  Remove-Item -Path $osqueryLogPath
+    # Remove file after read
+    Remove-Item -Path $osqueryLogPath
 
-  # Create web request
-  $uri = "http://192.168.10.44:9200/osquery/query"
-  $headers = @{ 
-    'Authorization' = $basicAuthValue
-  }
+    # Create web request
+    $uri = "http://192.168.10.44:9200/osquery/query-" + $(Get-Date -Format yyyy.MM.dd)
+    $headers = @{ 
+        'Authorization' = $basicAuthValue
+    }
 
-  $i = 0
-  ForEach ($line in $($logContent -split '\r?\n')) {
-    # Create the body
-    $jsonObject = $(ConvertFrom-Json -InputObject $line);
-    # Add the current time 
-    $jsonObject | add-member -Name "sent_at" -value $(Get-Date -Format s) -MemberType NoteProperty
+    $i = 0
+    ForEach ($line in $($logContent -split '\r?\n')) {
+        # Create the body
+        $jsonObject = $(ConvertFrom-Json -InputObject $line);
+        # Add the current time 
+        $jsonObject | add-member -Name "sent_at" -value $(Get-Date -Format s) -MemberType NoteProperty
 
-    # Send web request and store the response
-    $response = Invoke-WebRequest `
-      -UseBasicParsing $uri `
-      -Method Post `
-      -Headers $headers `
-      -ContentType 'application/json' `
-      -Body (ConvertTo-Json $jsonObject)
+        # Send web request and store the response
+        $response = Invoke-WebRequest `
+            -UseBasicParsing $uri `
+            -Method Post `
+            -Headers $headers `
+            -ContentType 'application/json' `
+            -Body (ConvertTo-Json $jsonObject)
 
-    # Counter
-    # Write-Host $i
-    $i = $i + 1
-    Write-Host $i
+        # Counter
+        # Write-Host $i
+        $i = $i + 1
+        Write-Host $i
 
-    # Log the response
-    Add-Content $scriptLogPath $(MakeLogObject -loggable $response.Content) 
-  }
+        # Log the response
+        WriteLog -scriptLogPath $scriptLogPath -loggable $response.Content 
+    }
 
-  Add-Content $scriptLogPath $(MakeLogObject -loggable $i ) 
-  Add-Content $scriptLogPath $(MakeLogObject -loggable 'run_end') 
+    WriteLog -scriptLogPath $scriptLogPath -loggable $i
+    WriteLog -scriptLogPath $scriptLogPath -loggable 'run_end' 
 }
 
-#SendToElasticSearch -scriptLogPath $macScriptLogPath -osqueryLogPath $macOsqueryLogPath
-SendToElasticSearch -scriptLogPath $windowsScriptLogPath -osqueryLogPath $windowsOsqueryLogPath
+# Add this as parameter to automatically execute for the correct OS version
+# SendToElasticSearch -OSVersion 'ubuntu'
